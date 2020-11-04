18a508d7d566f8a38f17fb909fc85e7b421db6f2
Sebastián Díaz Obando <sebascout01182@gmail.com>
2020-11-04 15:11:41 -0600

    EmpezarCiudad

--- NodoAVL.hpp
+++ NodoAVL.hpp
@@ -7,166 +7,120 @@
 #include <stdlib.h>
 using namespace std;
 
-class Node{  
+class NodoAVL{  
     public: 
-    int key;
+    int CodCiudad;
 	string nombre;  
-    Node *left;  
-    Node *right;  
-    int height;  
+    NodoAVL *izquierda;  
+    NodoAVL *derecha;  
+    int FB;  
 };  
-  
-// A utility function to get maximum 
-// of two integers  
-int max(int a, int b);  
-  
-// A utility function to get the  
-// height of the tree  
-int height(Node *N)  
-{  
+
+//Funcion para obtener el balance de un nodo
+int balanceo(NodoAVL *N){  
     if (N == NULL)  
         return 0;  
-    return N->height;  
+    return FB(N->izquierda) - FB(N->derecha);  
+}  
+
+int FB(NodoAVL *N){  
+    if (N == NULL)  
+        return 0;  
+    return N->FB;  
 }  
   
-// A utility function to get maximum 
-// of two integers  
-int max(int a, int b)  
-{  
+//Obtiene el numero mayor para identificar a la hora de ingresar en el arbol
+int numeroMayor(int a, int b); 
+
+int numeroMayor(int a, int b){  
     return (a > b)? a : b;  
 }  
+ 
+NodoAVL* new NodoAVL(int codCiudad,string pnombre){  
+    NodoAVL* NodoAVL = new NodoAVL(); 
+    NodoAVL->codCiudad = codCiudad;
+	NodoAVL->nombre = pnombre;  
+    NodoAVL->izquierda = NULL;  
+    NodoAVL->derecha = NULL;  
+    NodoAVL->FB = 1; 
+    return(NodoAVL);  
+}  
+   
+NodoAVL *RotacionDerecha(NodoAVL *y)  {  
+    NodoAVL *x = y->izquierda;  
+    NodoAVL *T2 = raizN->derecha;  
   
-/* Helper function that allocates a  
-   new node with the given key and  
-   NULL left and right pointers. */
-Node* newNode(int key,string pnombre)  
-{  
-    Node* node = new Node(); 
-    node->key = key;
-	node->nombre = pnombre;  
-    node->left = NULL;  
-    node->right = NULL;  
-    node->height = 1; // new node is initially 
-                      // added at leaf  
-    return(node);  
+    raizN->derecha = y;  
+    y->izquierda = T2;  
+  
+    y->FB = numeroMayor(FB(y->izquierda), 
+    FB(y->derecha)) + 1;  
+    raizN->FB = numeroMayor(FB(raizN->izquierda), 
+    FB(raizN->derecha)) + 1;
+    
+    return raizN;  
+}  
+   
+NodoAVL *RotacionIzquierda(NodoAVL *x) {  
+    NodoAVL *raizN = x->derecha;  
+    NodoAVL *T2 = raizN->izquierda;  
+   
+    raizN->izquierda = x;  
+    x->derecha = T2;  
+   
+    x->FB = numeroMayor(FB(x->izquierda),     
+    FB(x->derecha)) + 1;  
+    raizN->FB = numeroMayor(FB(raizN->izquierda),  
+    FB(raizN->derecha)) + 1;  
+  
+    return raizN;  
 }  
   
-// A utility function to right 
-// rotate subtree rooted with y  
-// See the diagram given above.  
-Node *rightRotate(Node *y)  
-{  
-    Node *x = y->left;  
-    Node *T2 = x->right;  
+
+NodoAVL* insertarnodoAVL(NodoAVL* nodo, int codCiudad,string nombre)  {  
+	//Si la raiz es nula
+    if (nodo == NULL){
+    	return(new NodoAVL(codCiudad,nombre));  
+	}//else
+	if (codCiudad < nodo->codCiudad){
+    	nodo->izquierda = insertarnodoAVL(nodo->izquierda, codCiudad ,nombre);  
+	}else if (codCiudad > nodo->codCiudad){
+    	nodo->derecha = insertarnodoAVL(nodo->right, codCiudad,nombre);  
+	}else{
+		return nodo;  
+	}   
+    nodo->FB = 1 + numeroMayor(FB(nodo->izquierda),  
+    FB(nodo->derecha));  
   
-    // Perform rotation  
-    x->right = y;  
-    y->left = T2;  
-  
-    // Update heights  
-    y->height = max(height(y->left), 
-                    height(y->right)) + 1;  
-    x->height = max(height(x->left), 
-                    height(x->right)) + 1;  
-  
-    // Return new root  
-    return x;  
-}  
-  
-// A utility function to left  
-// rotate subtree rooted with x  
-// See the diagram given above.  
-Node *leftRotate(Node *x)  
-{  
-    Node *y = x->right;  
-    Node *T2 = y->left;  
-  
-    // Perform rotation  
-    y->left = x;  
-    x->right = T2;  
-  
-    // Update heights  
-    x->height = max(height(x->left),     
-                    height(x->right)) + 1;  
-    y->height = max(height(y->left),  
-                    height(y->right)) + 1;  
-  
-    // Return new root  
-    return y;  
-}  
-  
-// Get Balance factor of node N  
-int getBalance(Node *N)  
-{  
-    if (N == NULL)  
-        return 0;  
-    return height(N->left) - height(N->right);  
-}  
-  
-// Recursive function to insert a key 
-// in the subtree rooted with node and 
-// returns the new root of the subtree.  
-Node* insert(Node* node, int key,string nombre)  
-{  
-    /* 1. Perform the normal BST insertion */
-    if (node == NULL)  
-        return(newNode(key,nombre));  
-  
-    if (key < node->key)  
-        node->left = insert(node->left, key,nombre);  
-    else if (key > node->key)  
-        node->right = insert(node->right, key,nombre);  
-    else // Equal keys are not allowed in BST  
-        return node;  
-  
-    /* 2. Update height of this ancestor node */
-    node->height = 1 + max(height(node->left),  
-                        height(node->right));  
-  
-    /* 3. Get the balance factor of this ancestor  
-        node to check whether this node became  
-        unbalanced */
-    int balance = getBalance(node);  
-  
-    // If this node becomes unbalanced, then  
-    // there are 4 cases  
-  
-    // Left Left Case  
-    if (balance > 1 && key < node->left->key)  
-        return rightRotate(node);  
-  
-    // Right Right Case  
-    if (balance < -1 && key > node->right->key)  
-        return leftRotate(node);  
-  
-    // Left Right Case  
-    if (balance > 1 && key > node->left->key)  
-    {  
-        node->left = leftRotate(node->left);  
-        return rightRotate(node);  
+    int balance = balanceo(nodo);  //Esta variable sera la del balance
+    
+    //Casos de las rotaciones
+    //ROTACION IZQUIERDA 
+    if (balance > 1 && codCiudad < nodo->izquierda->CodCiudad){
+    	return RotacionDerecha(nodo);  
+	}
+    // ROTACION DERECHA
+    if (balance < -1 && codCiudad > nodo->derecha->codCiudad){
+    	return RotacionIzquierda(node);  
+	} 
+    //ROTACION DOBLE IZQUIERDA 
+    if (balance > 1 && codCiudad > nodo->izquierda->codCiudad){  
+        nodo->izquierda = RotacionIzquierda(nodo->izquierda);  
+        return RotacionDerecha(nodo);  
+    } 
+    //ROTACION DOBLE DERECHA
+    if (balance < -1 && codCiudad < nodo->derecha->codCiudad){  
+        nodo->derecha = RotacionDerecha(nodo->derecha);  
+        return RotacionIzquierda(nodo);  
     }  
   
-    // Right Left Case  
-    if (balance < -1 && key < node->right->key)  
-    {  
-        node->right = rightRotate(node->right);  
-        return leftRotate(node);  
-    }  
-  
-    /* return the (unchanged) node pointer */
-    return node;  
-}  
-  
-// A utility function to print preorder  
-// traversal of the tree.  
-// The function also prints height  
-// of every node  
-void preOrder(Node *root)  
-{  
-    if(root != NULL)  
-    {  
-        cout <<root->key<<"-"<<root->nombre<<"->";  
-        preOrder(root->left);  
-        preOrder(root->right);  
+    return nodo;  
+} 
+
+void preOrder(NodoAVL *raiz)  {  
+    if(raiz != NULL)  {  
+        cout <<raiz->CodCiudad<<"-"<<raiz->nombre<<"->";  
+        preOrder(raiz->izquierda);  
+        preOrder(raiz->derecha);  
     }  
 }  
